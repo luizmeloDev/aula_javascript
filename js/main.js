e3b57284b7a7acb7935827ec7b872660276bdef2
@luizmeloDev <luizmelodev@gmail.compush>
2020-11-16 18:24:30 -0300

    Signed-off-by: @luizmeloDev <luizmelodev@gmail.compush>

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/* no arquivo main onde ficara a estrutura javascrip
+ultilizaremos variaveis com o comando alert e concatenização 
+de uma string*/
+var name= "Luiz Melo";
+var age=35;
+var age2=10;
+var phrase=(" the Japan is the best country over all the world")
+//alert(name +" Seja bem vindo!");
+//alert(name + " have " + age + " year old." );
+//alert(age + age2) 
+
+//O consol.log interage direto com o usuario
+
+/*O replace tem a funcao dentro do console.log 
+de substituir uma qualquer string pedida*/
+console.log(phrase);// deixa tudo em maiuscula
+console.log(name);
+//console.log(phrase.toUpperCase()); toUpperCase para letra menuscula
+console.log(phrase.toLocaleLowerCase());// toLocaLowerCase para letras maiuscula 
+console.log(age + age2);
+//console.log(phrase.replace("Japan", "Brazil"));
+//alert(phrase.replace("Japan","Brazil"));
+
