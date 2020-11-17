20a77248188493ec6e5c86a9c41618ca2b76e762
@luizmeloDev <luizmelodev@gmail.compush>
2020-11-17 07:02:20 -0300

    Aula atualizada

--- js/main.js
+++ js/main.js
@@ -1,23 +1,103 @@
-/* no arquivo main onde ficara a estrutura javascrip
+/*no arquivo main onde ficara a estrutura javascrip
 ultilizaremos variaveis com o comando alert e concatenização 
 de uma string*/
-var name= "Luiz Melo";
+/*var name= "Luiz Melo";
 var age=35;
 var age2=10;
 var phrase=(" the Japan is the best country over all the world")
-//alert(name +" Seja bem vindo!");
-//alert(name + " have " + age + " year old." );
-//alert(age + age2) 
+alert(name +" Seja bem vindo!");
+alert(name + " have " + age + " year old." );
+alert(age + age2)*/
 
 //O consol.log interage direto com o usuario
 
 /*O replace tem a funcao dentro do console.log 
 de substituir uma qualquer string pedida*/
-console.log(phrase);// deixa tudo em maiuscula
+
+/*console.log(phrase);// deixa tudo em maiuscula
 console.log(name);
-//console.log(phrase.toUpperCase()); toUpperCase para letra menuscula
+console.log(phrase.toUpperCase()); toUpperCase para letra menuscula
 console.log(phrase.toLocaleLowerCase());// toLocaLowerCase para letras maiuscula 
 console.log(age + age2);
-//console.log(phrase.replace("Japan", "Brazil"));
-//alert(phrase.replace("Japan","Brazil"));
+console.log(phrase.replace("Japan", "Brazil"));
+alert(phrase.replace("Japan","Brazil"));*/
 
+//Array e Dicionario
+
+/*var fruit = [{nome:"maça", cor:"vermelha"},{nome: "uva", cor:"roxa"}]
+console.log(fruit);
+alert(fruit[1].nome);*/
+
+//var list = ["maçã","pera", "laranja"];
+/*var fruit = {nome:"maça", cor:"vermelha"};
+console.log(fruit.nome);
+alert(fruit.cor);*/
+
+
+//console.log(list[0]);
+//console.log(list.join(" - ")); // junta e separa caracteres
+//console.log(list.toString()[0]);// transforma a lista em uma string
+//console.log(list.reverse());//inverte a lista
+//list.pop(); // retira item da lista
+//console.log(list.length);// conta os itens na lista
+//list.push("uva"); // acrecenta item na lista
+//console.log(list); // mostra os itens na lista
+//console.log(list[2]);// localiza item na lista
+
+//Condicionais, laços de repetição
+
+/*var age=18;
+if (age >= 18){
+    alert("Maior de idade")
+}
+else{
+    alert("Menor de idade")
+}*/
+
+
+/*var age= prompt ("How old are you?");
+if (age >= 18){
+    alert("older");
+}
+else{
+    alert("minor");
+}*/
+
+//Estrutura de repetição
+/*var count=0;
+while (count <=5)
+{
+console.log(count)
+count=count + 1;
+}*/
+
+/*var count=0;
+while (count <=5)
+{
+console.log(count)
+count++;// mesma coisa que count=count + 1
+}*/
+
+/*var count=0;
+while (count <=5)
+{
+console.log(count)
+alert(count);
+count=count + 1;
+}*/
+
+/*var count;
+for(count=0; count <=5; count++){
+alert(count);
+};*/
+
+//trabalhando com Datas
+/*var d= new Date();
+alert(d);*/
+
+//var d= new Date();
+//alert(d.getMonth()+1);
+//alert(d.getDay());
+//alert(d.getHours());
+//alert(d.getMinutes());
+//alert(d.getSeconds());
